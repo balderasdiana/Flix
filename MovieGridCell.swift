ab73416297c1cc539f8fc93cf5223757020a4b37
Diana Balderas <balderasdiana27@gmail.com>
2019-10-21 21:55:04 -0700

    changes :)

---
+++ MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  
+//
+//  Created by Diana Balderas on 10/21/19.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
